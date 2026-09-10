72e898f1df7bef061d401e72a7ab53cd6a241c72
Feng Zhang <fzhang@ameslab.gov>
2026-09-10 15:07:22 -0700

    format fix on pem.py

--- jobs/pem.py
+++ jobs/pem.py
@@ -81,12 +81,11 @@ class pem_simulator(lammpsJobGroup):
                     self.write_script(job._script, general, T)
                 self._jobList.append(job)
 
-
     def write_script(self, scriptFile, general, T):
         """
         Write a LAMMPS input script for nucleation simulations using the
         Persistent Embryo Method (PEM).
-    
+
         Parameters
         ----------
         scriptFile : str
@@ -96,7 +95,10 @@ class pem_simulator(lammpsJobGroup):
         T : float
             Target temperature.
         """
-        baro_style = f"iso {general.pressure} {general.pressure} {general.Pdamp}"
+        baro_style = f"iso {
+            general.pressure} {
+            general.pressure} {
+            general.Pdamp}"
         f = open(scriptFile, 'wt')
         f.write("# Persistent Embryo Method nucleation simulation\n")
         f.write("\n")
@@ -105,19 +107,21 @@ class pem_simulator(lammpsJobGroup):
         f.write("atom_style      atomic\n")
         f.write("atom_modify     map array\n")
         f.write("\n")
-    
+
         f.write(f"read_data       {self._datain}\n")
         f.write("\n")
-    
+
         f.write(general.pair._cmd)
         if general.neighbor is not None:
             f.write(f"neighbor        {general.neighbor}\n")
             f.write(f"neigh_modify    {general.neigh_modify}\n")
         f.write("\n")
-    
+
         f.write("# set constant\n")
         f.write("   # MD\n")
-        f.write(f"   variable        vseed equal {np.random.randint(1000000)}\n")
+        f.write(
+            f"   variable        vseed equal {
+                np.random.randint(1000000)}\n")
         f.write(f"   variable        temp equal {T}\n")
         f.write(f"   variable        htemp equal {self._Tmelt}\n")
         f.write("   variable        nloop equal 10000\n")
@@ -128,10 +132,11 @@ class pem_simulator(lammpsJobGroup):
         f.write("   # spring constant k=kspr0*scaler, while scaler=(Nsc-Nsolid)/Nsc\n")
         f.write("   variable        kspr0 equal 100\n")
         f.write("   variable        time_window equal 3000 # *1fs, the time window to change the spring constant\n")
-        f.write("   variable        dump_window equal 200 # *1fs, the time window to dump trajectory\n")
+        f.write(
+            "   variable        dump_window equal 200 # *1fs, the time window to dump trajectory\n")
         f.write(f"   variable        Nsc equal {self._Nsc}\n")
         f.write(f"   variable        Nbreak equal {5 * self._Nsc}\n\n")
-    
+
         f.write("# thermo\n")
         if general.timestep is not None:
             f.write(f"timestep        {general.timestep}\n")
@@ -141,9 +146,9 @@ class pem_simulator(lammpsJobGroup):
         f.write(f"thermo          {general.thermo}\n")
         f.write(
             "thermo_style    custom step temp etotal pe ke press enthalpy vol c_scom[1] c_scom[2] c_scom[3]\n\n")
-    
+
         f.write("# generate initial liquid\n")
-        #f.write("reset_atoms id\n")
+        # f.write("reset_atoms id\n")
         f.write("velocity all create ${temp} ${vseed} rot yes dist gaussian\n")
         f.write(
             f"fix            anneal liquid npt temp ${{htemp}} ${{htemp}} {general.Tdamp} {baro_style}\n")
@@ -154,7 +159,7 @@ class pem_simulator(lammpsJobGroup):
         f.write("run            1000\n")
         f.write("unfix          cool\n")
         f.write("\n")
-    
+
         f.write("variable        scaler equal 0.01\n")
         f.write("variable        kspr equal ${kspr0}*${scaler}\n")
         f.write("# equilibrium\n")
@@ -168,60 +173,62 @@ class pem_simulator(lammpsJobGroup):
         f.write("unfix           com_seed\n")
         f.write("unfix           spring\n")
         f.write("\n")
-    
+
         f.write("# nucleation start\n")
         f.write("reset_timestep  0\n")
         f.write("\n")
-    
+
         f.write("# compute Q6.Q6*\n")
         f.write("thermo          100\n")
         f.write(
             "compute         Q6 all orientorder/atom degrees 1 6 components 6 nnn NULL cutoff ${rcut}\n")
-        f.write("compute         QConn all coord/atom orientorder Q6 ${qq6_cut}\n")
+        f.write(
+            "compute         QConn all coord/atom orientorder Q6 ${qq6_cut}\n")
         f.write("compute         sum_conn all reduce sum c_QConn\n")
         f.write("variable        q6Coor atom \"c_QConn >= v_connect_cut\"\n")
         f.write(
             "group           solid dynamic all var q6Coor every ${time_window}\n")
         f.write("compute         clustering solid cluster/atom ${rcut}\n")
         f.write("compute         cluster_index solid chunk/atom c_clustering\n")
-        f.write("compute         cluster_size  solid property/chunk cluster_index count\n")
+        f.write(
+            "compute         cluster_size  solid property/chunk cluster_index count\n")
         f.write("variable        Nsolid equal max(c_cluster_size)\n")
         f.write(
             "thermo_style    custom step temp etotal pe ke press enthalpy vol c_scom[1] c_scom[2] c_scom[3] c_sum_conn v_Nsolid\n")
         f.write("run 0\n")
         f.write("print \"ok\"\n")
         f.write("\n")
-    
+
         f.write("# NPT\n")
         f.write(
             f"fix             ncl all npt temp  ${{temp}} ${{temp}} {general.Tdamp} {baro_style}\n")
         f.write("fix             nodrift all momentum 1 linear 1 1 1\n")
         f.write("fix             com_seed seed recenter INIT INIT INIT shift all\n")
         f.write("\n")
-    
+
         f.write("# compute position\n")
         f.write("compute         uwpos all property/atom xu yu zu\n")
         f.write("\n")
-    
+
         f.write("# define equal-style variables for COM components\n")
         f.write("variable        scomx equal c_scom[1]\n")
         f.write("variable        scomy equal c_scom[2]\n")
         f.write("variable        scomz equal c_scom[3]\n")
         f.write("\n")
-    
+
         f.write("# now define per-atom variables relative to COM\n")
         f.write("variable        px  atom c_uwpos[1]-v_scomx\n")
         f.write("variable        py  atom c_uwpos[2]-v_scomy\n")
         f.write("variable        pz  atom c_uwpos[3]-v_scomz\n")
         f.write("\n")
-    
+
         f.write("# store initial seeds' positions\n")
         f.write("fix             pseed0 seed store/state 0 v_px v_py v_pz\n")
         f.write("\n")
-    
+
         f.write("variable        scaler equal 0.1\n")
         f.write("\n")
-    
+
         f.write("# loop starts\n")
         f.write("label           loop_mark\n")
         f.write("   variable        j loop ${nloop}\n")
@@ -230,8 +237,10 @@ class pem_simulator(lammpsJobGroup):
         f.write("   variable        sprx atom ${kspr}*(f_pseed0[1]-v_px)\n")
         f.write("   variable        spry atom ${kspr}*(f_pseed0[2]-v_py)\n")
         f.write("   variable        sprz atom ${kspr}*(f_pseed0[3]-v_pz)\n")
-        f.write("   fix             spring seed addforce v_sprx v_spry v_sprz every 1\n")
-        f.write("   thermo_style    custom step temp etotal pe ke press enthalpy vol v_Nsolid  v_kspr\n")
+        f.write(
+            "   fix             spring seed addforce v_sprx v_spry v_sprz every 1\n")
+        f.write(
+            "   thermo_style    custom step temp etotal pe ke press enthalpy vol v_Nsolid  v_kspr\n")
         f.write(
             "   dump            2 all custom ${dump_window} all.*.atom id type xs ys zs vx vy vz c_QConn\n")
         f.write("   dump_modify     2 sort id\n")
@@ -250,5 +259,5 @@ class pem_simulator(lammpsJobGroup):
         f.write("   jump            SELF loop_mark\n")
         f.write("\n")
         f.write("label           break\n")
-    
+
         f.close()
